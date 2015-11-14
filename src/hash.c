ece91490fa82a8ad28f13aad06d21464999ba25b
Sam Hatchett <samhatchett@gmail.com>
2015-11-14 16:46:35 -0500

    fixes cross-platform availability issue

--- src/hash.c
+++ src/hash.c
@@ -63,7 +63,7 @@ int ENHashTableInsert(ENHashTable *ht, char *key, int data)
   }
   size_t len = strlen(key) + 1;
   entry->key = calloc(len, sizeof(char));
-  strlcpy(entry->key, key, len);
+  strncpy(entry->key, key, len);
   entry->data = data;
   entry->next = ht[i];
   ht[i] = entry;
